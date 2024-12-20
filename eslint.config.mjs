e173d20ca43f153be09d984282a103fdef5f69b9
Gilson Nyirimana <gilson.nyirimana@auctria.com>
2024-12-20 16:34:28 -0500

    Fix: ESLint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import withNuxt from './playground/.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/no-v-html': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+    'vue/no-restricted-syntax': [
+      'error',
+      {
+        selector: 'VElement[name="a"]',
+        message: 'Use NuxtLink instead.',
+      },
+      {
+        selector: 'VElement[name="router-link"]',
+        message: 'Use NuxtLink instead.',
+      },
+    ],
+    'vue/block-order': ['error', {
+      order: ['script', 'template', 'style'],
+    }],
+    'vue/block-lang': ['error', {
+      script: { lang: 'ts' },
+    }],
+  },
+})
